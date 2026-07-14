1e1cb0a06f6a8eed770db12737e3660824f085c0
Rupendra Sharma <rupendra1817@mail.com>
2026-07-14 21:02:40 +0530

    Removed older project

--- App.js
+++
@@ -1,4 +0,0 @@
-const heading = React.createElement("h1", { id: "heading", xyz: "test" }, "Welcome to ReactJS world!");
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
